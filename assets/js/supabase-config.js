a98e57f61ab0f43635835ace0b483908d3acfbc2
clesterdpt-lgtm <clesterdpt@gmail.com>
2026-06-24 22:11:54 -0400

    Enable Supabase backend config

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,6 +1,6 @@
 window.RB_SUPABASE_CONFIG = {
-  enabled: false,
-  url: "",
-  anonKey: "",
-  emailRedirectTo: "",
+  enabled: true,
+  url: "https://qzvrrpzmalskayushbrv.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6dnJycHptYWxza2F5dXNoYnJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzNDQ5NDcsImV4cCI6MjA5NzkyMDk0N30.K3tr3I7Sa-ldLUz2_a8qGPLEPwaH1ZzFmDcSDf7WndQ",
+  emailRedirectTo: "https://clesterdpt-lgtm.github.io/rainbot-gaming/community.html",
 };

16d31d4e30a3c94e39d8b9c9dab8bf5bd8b7c658
clesterdpt-lgtm <clesterdpt@gmail.com>
2026-07-03 22:58:31 -0400

    Correct Scrap Circuit steering mapping

--- assets/js/scrap-circuit/main.js
+++ assets/js/scrap-circuit/main.js
@@ -1654,7 +1654,7 @@
       steer = touchMove.x;
     }
     car.throttle = Math.max(-1, Math.min(1, throttle));
-    car.steer = Math.max(-1, Math.min(1, -steer));
+    car.steer = Math.max(-1, Math.min(1, steer));
     car.drift = !!(keys.ShiftLeft || keys.ShiftRight || touchMove.drift);
     car.wantFire = !!(keys.Space || mouseHeld || touchFireHeld);
   }
